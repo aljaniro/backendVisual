ba75ca9daa97f36617c31d2ecbf14e06bc8d54d8
Alvaro Nieto <anieto@fundacionlm.org>
2022-03-26 11:34:52 -0500

    commit inicial

---
+++ API/Controllers/LugaresController.cs
@@ -0,0 +1,54 @@
+﻿using Infraestructura.Datos;
+using Core.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LugaresController : ControllerBase
+    {
+        private readonly AppDbContext db;
+
+        public LugaresController(AppDbContext db)
+        {
+            this.db = db;
+        }
+
+        // GET: api/<LugaresController>
+        [HttpGet]
+        public async Task<ActionResult<List<Lugar>>> getlugares()
+        {
+            var lugares = await this.db.lugar.ToListAsync();
+            return Ok(lugares);
+        }
+
+        // GET api/<LugaresController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Lugar>> Get(int id)
+        {
+            return await this.db.lugar.FindAsync(id);
+        }
+
+        // POST api/<LugaresController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<LugaresController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<LugaresController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
